ad0ca72781c6bfff12ff23cfc6feedb31fbb0ebf
Nir Berko <nirberko@gmail.com>
2021-01-17 20:55:38 +0200

    sticky navigation

--- packages/client/src/components/Navigation/Navigation.tsx
+++ packages/client/src/components/Navigation/Navigation.tsx
@@ -14,8 +14,8 @@ export const Navigation = () => {
   const history = useHistory();
 
   return (
-    <nav className="min-h-full flex p-6">
-      <ul className="w-full">
+    <nav className="sticky min-h-full p-6">
+      <ul className="w-full sticky top-6">
         {items.map(({ icon, route, label }, i) => (
           <li
             className="flex flex-row items-center h-14 px-4 text-gray-600 rounded-lg hover:bg-gray-100 cursor-pointer"
